e27b8c5b83c321a50615019d56a3395da592ded4
Suresh5992 <107224549+Suresh5992@users.noreply.github.com>
2024-12-14 14:23:16 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 sudo apt update -y
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
+docker info 
+
+sudo chmod 777 /var/run/docker.sock
+
+docker info 
+
+#check insecure Reprository configured with ip check it
+#Docker has been installed 
+
+# now creating the container for Sonarqube 
+
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube 
+ 
+
+# [host port] 9000 traffic automatically get transfer to container of this port 9000
+# mutiple port also you can do  -p 9092:9092 sonarqube-> image 
+
+# check the container running state 
+
+docker conatiner ls 
+
+
+# till here is sufficient 
+
+
+# if you are confustion 
+#youtube link :  https://www.youtube.com/watch?v=8KZi7KBpk0I&t=3s 
+#tiem stamp :  34 min play 
+
+
+----------------------------------------------------------------------------------
 sudo apt update -y
 
 apt-cache policy docker-ce -y
@@ -15,4 +45,4 @@ sudo apt install docker-ce -y
 
 #sudo systemctl status docker
 
-sudo chmod 777 /var/run/docker.sock
+#sudo chmod 777 /var/run/docker.sock
